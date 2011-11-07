b3752f1476de0cb0e1e67c3f64db7e963461232b
James Byatt <grumpyjames@gmail.com>
2011-11-07 22:37:40 +0000

    refactor : replaceThenJoin ftw

--- src/main/scala/org/grumpysoft/Inliner.scala
+++ src/main/scala/org/grumpysoft/Inliner.scala
@@ -2,65 +2,63 @@ package org.grumpysoft
 
 import java.io.File
 import scala.sys.process.Process
+import java.lang.String
 
 object Inliner {
   import FileFinder.relativeFinder
 
+  type Finder = String => File
+
   def inline(template: String) : String = {
-    val lines = template.lines
-    lines.map(doReplacements _).reduceLeft(_ + "\n" + _)
-  }
-
-  def defaultFileFinder: String => File = {
-    fileName => FileFinder.file(fileName)
-  }
-
-  def fileAsLines(file: File) = {
-    scala.io.Source.fromFile(file).getLines
+    replaceThenJoin(template.lines, defaultFileFinder)
   }
 
   def inline(templateFile: File) : String = {
-    fileAsLines(defaultFileFinder(templateFile.getPath))
-      .map(doReplacements(_, relativeFinder(templateFile)))
-      .reduceLeft(_ + "\n" + _)
+    replaceThenJoin(fileAsLines(defaultFileFinder(templateFile.getPath)), relativeFinder(templateFile))
   }
 
-  def doReplacements(line: String) : String = {
-    doReplacements(line, defaultFileFinder)
+  private def defaultFileFinder : Finder = {
+    fileName => FileFinder.file(fileName)
   }
 
-  def doGitReplacement(gitCmd: String) : String = {
+  private def fileAsLines(file: File) : Iterator[String] = {
+    scala.io.Source.fromFile(file).getLines()
+  }
+
+  private def doGitReplacement(gitCmd: String) : String = {
     // assumes: . is a git directory
     // format like src/test/data/many_versioned_file@428de6
     val fileAndVersion = gitCmd.split("@")
-    Process("git show " + fileAndVersion(1) + ":" + fileAndVersion(0))
-      .lines.map(doReplacements(_, relativeFinder(new File(fileAndVersion(0))))).reduceLeft(_ + "\n" + _).stripLineEnd
+    val filePath: String = fileAndVersion(0)
+    val showInvocation: String = "git show " + fileAndVersion(1) + ":" + filePath
+    // annoyingly git ends with a newline, so we have to strip it.
+    replaceThenJoin(Process(showInvocation).lines, relativeFinder(new File(filePath))).stripLineEnd
   }
 
-  def doBashReplacement(bashCmd: String, finder: String => File) : String = {
+  private def doBashReplacement(bashCmd: String, finder: Finder) : String = {
     val delimitedCmd: Array[String] = bashCmd.split('`')
     val shellCmd = delimitedCmd(1)
-    Process(shellCmd).lines.map(doReplacements(_, finder)).reduceLeft(_ + "\n" + _) + delimitedCmd(2)
+    replaceThenJoin(Process(shellCmd).lines, finder) + delimitedCmd(2)
   }
 
-  def doReplacements(line: String, fileFinder: String => File) : String = {
-    val splitByColon = line.split("!inline:")
-    if (splitByColon.size > 1) {
-      val inlineInvocation = splitByColon(1)
-      val splitByPlus = line.split('+')
-      if (splitByPlus.size > 1) {
-        val invocationType: String = splitByPlus(0).split(':')(1)
-        invocationType match {
-          case "git" => splitByColon(0) + doGitReplacement(splitByPlus(1))
-          case "bash" => splitByColon(0) + doBashReplacement(splitByPlus(1), fileFinder)
-        }
+  private def replaceThenJoin(lines: TraversableOnce[String], finder: Finder) : String = {
+    lines.map(doReplacements(_, finder)).reduceLeft(_ + "\n" + _)
+  }
 
-      } else {
-        val toInline = fileFinder(inlineInvocation)
-        splitByColon(0) + fileAsLines(toInline).map(doReplacements(_, FileFinder.relativeFinder(toInline))).reduceLeft(_ + "\n" + _)
-      }
-    } else {
-      line
+  private def doReplacements(line: String, fileFinder: Finder) : String = {
+    val splitByColon = line.split("!inline:")
+    if (splitByColon.size == 1) return line
+
+    val splitByPlus = line.split('+')
+    if (splitByPlus.size == 1) {
+      val toInline = fileFinder(splitByColon(1))
+      return splitByColon(0) + fileAsLines(toInline).map(doReplacements(_, FileFinder.relativeFinder(toInline))).reduceLeft(_ + "\n" + _)
+    }
+
+    val invocationType: String = splitByPlus(0).split(':')(1)
+    invocationType match {
+      case "git" => splitByColon(0) + doGitReplacement(splitByPlus(1))
+      case "bash" => splitByColon(0) + doBashReplacement(splitByPlus(1), fileFinder)
     }
   }
 }
